17fb4b4da7f590ab3b1196336045c8dbddaada5e
DanielSantini <danielsantini36@gmail.com>
2024-02-28 17:09:31 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/ArticoliController.cs
@@ -0,0 +1,12 @@
+﻿using DittaScarpe.Models;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using static DittaScarpe.Models.Articolo;
+
+namespace DittaScarpe.Controllers
+{
+    public class ArticoliController : Controller
+    {
+        
+    }
+}
---
+++ Models/Articolo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DittaScarpe.Models
+{
+    public class Articolo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il nome dell'articolo è obbligatorio.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Il prezzo dell'articolo è obbligatorio.")]
+        public decimal Prezzo { get; set; }
+
+        [Required(ErrorMessage = "La descrizione dell'articolo è obbligatoria.")]
+        public string Descrizione { get; set; }
+
+        [Display(Name = "Immagine di copertina")]
+        public string ImmagineCopertina { get; set; }
+
+        [Display(Name = "Immagine aggiuntiva 1")]
+        public string ImmagineAggiuntiva1 { get; set; }
+
+        [Display(Name = "Immagine aggiuntiva 2")]
+        public string ImmagineAggiuntiva2 { get; set; }
+    }
+}
